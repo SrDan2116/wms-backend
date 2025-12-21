77bb5d0bf2cc3c623fbcb030c934d37eaaa871bc
Mdro123 <daniel.aliaga.h37@gmail.com>
2025-12-21 01:04:07 -0500

    Inicio Proyecto Fitness - Reinicio total

---
+++ src/test/java/com/aliaga/wms/FittrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliaga.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
